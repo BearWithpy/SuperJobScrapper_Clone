5147715863bde084e6bd25452127e146b966ae7c
박준수 <pksjmh5295@gmail.com>
2020-07-13 13:36:41 +0900

    Make flask file | FakeDB,stackoverflow scrapper

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("Superscrapper")
+
+db = {}
 
 
 @app.route("/")
 def home():
-    return "Hello! Welcome to mi casa!"
+    return render_template("index.html")
 
 
-@app.route("/contact")
-def contact():
-    return "Contact ME"
+@app.route("/report")
+def report():
+    # print(request.args.get("word"))
+    keyword = request.args.get("word")
+    if keyword:
+        keyword = keyword.lower()
+        fromDB = db.get(keyword)
+
+        if fromDB:
+            jobs = fromDB
+        else:
+            jobs = get_jobs(keyword)
+            db[keyword] = jobs
+
+    else:
+        return redirect("/")
+
+    return render_template("report.html", searchingBy=keyword, resultsNumber=len(jobs))
+
+
+# Hello! Welcome to mi casa!
+# @app.route("/<username>")
+# def contact(username):
+#     return f"Contact ME, {username}!"
 
 
 app.run(host="0.0.0.0", port=7000)
