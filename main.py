7ebd20351884fce442bc931c9f03b8e7550a94ba
박준수 <pksjmh5295@gmail.com>
2020-07-17 15:11:11 +0900

    Edit scrapper file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("Superscrapper")
 
@@ -17,10 +18,10 @@ def report():
     keyword = request.args.get("word")
     if keyword:
         keyword = keyword.lower()
-        fromDB = db.get(keyword)
+        existing_jobs = db.get(keyword)
 
-        if fromDB:
-            jobs = fromDB
+        if existing_jobs:
+            jobs = existing_jobs
         else:
             jobs = get_jobs(keyword)
             db[keyword] = jobs
@@ -28,7 +29,25 @@ def report():
     else:
         return redirect("/")
 
-    return render_template("report.html", searchingBy=keyword, resultsNumber=len(jobs))
+    return render_template(
+        "report.html", searchingBy=keyword, resultsNumber=len(jobs), jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        keyword = request.args.get("word")
+        if not keyword:
+            raise Exception()
+        keyword = keyword.lower()
+        jobs = db.get(keyword)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 
 # Hello! Welcome to mi casa!
